9b39978ed6a4563c42c5b12f882f287a43b84497
Athea DeLing <almdeling@gmail.com>
2022-08-09 16:29:21 -0700

    add on click
    co-authored-by: Helen Lehrer <hlehrer@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,42 @@
-function getRandom(min, max)){
+
+/* window.onload = function() {
+
+function getRandom(min, max){
   min = Math.ceil(0);
   max = Math.floor(255);
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-let r = getRandom(0, 255);
-let g = getRandom(0, 255);
-let b = getRandom(0,255);
 
-let body =document.querySelector("body")
+let r = getRandom(0, 255).toString();
+let g = getRandom(0, 255).toString();
+let b = getRandom(0,255).toString();
+let rgb = "rgb(".concat(r, g, b).concat(")");
+
+let body =document.querySelector("body");
+
 body.onmouseover = function() {
-  body.style.backgroundColor("rgb(getRandom(0, 255),getRandom(0, 255),getRandom(0, 255))")
-}
+  body.style.backgroundColor= "rgb";
+};
+
+}; */
+
+window.onload = function() {
+
+  function getRandom(min, max){
+    min = Math.ceil(0);
+    max = Math.floor(255);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+  
+  let r = getRandom(0, 255).toString();
+  let g = getRandom(0, 255).toString();
+  let b = getRandom(0,255).toString();
+  let rgb = "rgb(".concat(r, g, b).concat(")");
+  
+  let body =document.querySelector("body");
+  
+  body.onclick = function() {
+    body.style.backgroundColor= "rgb";
+  };
+  
+  };
